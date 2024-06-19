5c3a95bfa224b79868b6205f4c14712158adab80
Rupesh Shrestha <rupeshshrestha9818@gmail.com>
2024-06-19 21:52:31 +0545

    feat: added server timing middleware test case

---
+++ tests/ServerTiming/ServerTimingMiddlewareTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CoreFoundation\Tests\ServerTiming;
+
+use CoreFoundation\Facades\Services\ServerTimingFacadeService;
+use CoreFoundation\Http\Middlewares\ServerTimingMiddleware;
+use Illuminate\Http\Request;
+use Orchestra\Testbench\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class ServerTimingMiddlewareTest extends TestCase
+{
+    /** @test */
+    public function itAddServerTimingHeader(): void
+    {
+        $request = new Request;
+
+        $timing = new ServerTimingFacadeService(new Stopwatch());
+
+        $middleware = new ServerTimingMiddleware($timing);
+
+        $response = $middleware->handle($request, function ($req) {
+            return new Response();
+        });
+
+        $this->assertArrayHasKey('server-timing', $response->headers->all());
+    }
+}
