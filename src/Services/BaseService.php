7cc72894d52e8ee73ff3852c45dc3ab8b89fd1ba
Rupesh Shrestha <rupeshshrestha9818@gmail.com>
2024-06-22 23:02:33 +0545

    wip

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -2,6 +2,57 @@
 
 namespace CoreFoundation\Services;
 
+use CoreFoundation\Traits\HasEvent;
+use CoreFoundation\Manipulators\ObjectMutable;
+use CoreFoundation\Traits\HasCacheable;
+use Exception;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
 abstract class BaseService
 {
+    use HasCacheable;
+    use HasEvent;
+
+    protected ObjectMutable $objectMutable;
+
+    /**
+     * Validate with custom attributes data.
+     *
+     * Sometimes you need to validate data inside business logic, You can use this method to validate data
+     *
+     * @param array $data
+     * @param array $rules
+     * @param array $messages
+     * @param array $customAttributes
+     *
+     * @return array
+     */
+    public function validate(
+        array $data,
+        array $rules,
+        array $messages = [],
+        array $customAttributes = []
+    ): array {
+        try {
+            $validator = Validator::make($data, $rules, $messages, $customAttributes);
+            if ($validator->fails()) {
+                throw ValidationException::withMessages($validator->errors()->toArray());
+            }
+            $validated = $validator->validated();
+        } catch (Exception $exception) {
+            throw $exception;
+        }
+
+        return $validated;
+    }
+
+    public function before(mixed ...$data): mixed
+    {
+        dd($data);
+    }
+
+    public function after(mixed ...$data): mixed
+    {
+    }
 }
