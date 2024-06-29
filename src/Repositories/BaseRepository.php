afd93447e7ea22256b114d4cd0122c930ea8c5da
Rupesh Shrestha <rupeshshrestha9818@gmail.com>
2024-06-29 22:10:54 +0545

    feat: added filterable

--- src/Repositories/BaseRepository.php
+++ src/Repositories/BaseRepository.php
@@ -4,13 +4,12 @@ namespace CoreFoundation\Repositories;
 
 use Illuminate\Support\Arr;
 use CoreFoundation\Traits\HasEvent;
-use Illuminate\Pagination\Paginator;
 use CoreFoundation\Entities\BaseModel;
 use Illuminate\Foundation\Application;
 use Illuminate\Database\Eloquent\Builder;
 use CoreFoundation\Services\ModelFilterable;
 use Illuminate\Database\Eloquent\Collection;
-use CoreFoundation\Manipulators\ObjectMutable;
+use Illuminate\Contracts\Pagination\Paginator;
 use CoreFoundation\Services\RepositoryCacheManager;
 use CoreFoundation\Contracts\BaseRepositoryInterface;
 use CoreFoundation\Exceptions\ModelNotInstantiableException;
@@ -21,19 +20,13 @@ abstract class BaseRepository implements BaseRepositoryInterface
 
     protected BaseModel $model;
 
-    protected string $tableName;
-
-    protected int $perPage;
-    protected bool $isCached;
-    protected int $cacheTTl;
-    protected array $cacheAllowedMethods = [];
     protected array $coreConfig = [];
+    protected array $cacheAllowedMethods = [];
 
     public function __construct(
         protected Application $app,
-        protected RepositoryCacheManager $cacheManager,
         protected ModelFilterable $modelFilterable,
-        protected ObjectMutable $objectMutable,
+        protected RepositoryCacheManager $cacheManager,
     ) {
         $this->register();
     }
@@ -52,9 +45,13 @@ abstract class BaseRepository implements BaseRepositoryInterface
      */
     private function register(): void
     {
-        $modelInstance = !isset($this->model)
-            ? $this->app->make($this->setModel())
-            : null;
+        /**
+         * Singleton model instance inside service container.
+         * Reduce instantiation of same model object.
+         */
+        $this->app->singleton($this->setModel(), $this->setModel());
+
+        $modelInstance = $this->app->make($this->setModel());
 
         throw_unless(
             condition: $modelInstance instanceof BaseModel,
@@ -64,12 +61,11 @@ abstract class BaseRepository implements BaseRepositoryInterface
         );
 
         $this->model = $modelInstance;
-        $this->tableName = $this->model->getTable();
         $this->cacheManager->setModel($this->model);
         $this->coreConfig = config("core_foundation");
 
         $this->cacheAllowedMethods = Arr::get($this->coreConfig, "cache.cache_repository_methods");
-        $this->eventPrefix = class_basename($this);
+        $this->eventPrefix = $this->model->getTable();
         $this->eventDispatch = true;
     }
 
@@ -89,12 +85,13 @@ abstract class BaseRepository implements BaseRepositoryInterface
         $fetched = $this->cacheManager->make(
             relates: $relationship,
             callback: function () use ($filterable, $relationship, $columns) {
-                $rows = $this->model::query()
-                    ->select($columns)
+                $rows = $this->model::select($columns)
                     ->when($relationship, function (Builder $query) use ($relationship) {
                         $query->with($relationship);
                     });
-                return $this->modelFilterable->getFiltered($rows, $filterable, $relationship);
+                return $this->modelFilterable
+                    ->setModel($this->model)
+                    ->getFiltered($rows, $filterable, $relationship);
             },
             isCached: in_array(__FUNCTION__, $this->cacheAllowedMethods),
             identifier: [$filterable, $relationship],
--- src/Services/ModelFilterable.php
+++ src/Services/ModelFilterable.php
@@ -2,22 +2,167 @@
 
 namespace CoreFoundation\Services;
 
+use Closure;
+use CoreFoundation\Entities\BaseModel;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+use CoreFoundation\Manipulators\ObjectMutable;
+use Illuminate\Contracts\Pagination\Paginator;
 use CoreFoundation\Http\Requests\ValidateFilterableRequest;
 
 class ModelFilterable
 {
+    private array $queryOperators = [
+        "__gt_" => ">",
+        "__gte_" => ">=",
+        "__lt_" => "<",
+        "__lte_" => "<=",
+        "__eq_" => "=",
+        "__neq_" => "!=",
+        "__in_" => "IN",
+        "__nin_" => "NOT IN",
+        "__like_" => "LIKE",
+        "__nlike_" => "NOT LIKE",
+        "__null_" => "IS NULL",
+        "__nnull_" => "IS NOT NULL",
+        "__or_" => "OR",
+        "__and_" => "AND",
+    ];
+
+    private array $queryExpressions = [
+        "__or_*" => "OR",
+        "__and_*" => "AND",
+    ];
+
+    protected BaseModel $model;
+
     public function __construct(
-        protected ValidateFilterableRequest $validateFilterableRequest
+        protected ValidateFilterableRequest $validateFilterableRequest,
     ) {
     }
 
-    public function getFiltered(Builder $rows, array $filterable = [], array $relationship = []): object
+    public function setModel(BaseModel $model): self
     {
-        $data = $this->validateFilterableRequest->validated();
-        dd($data);
-        $rows->paginate();
-        return $rows;
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getFiltered(Builder $builder, array $requestFilters = []): Collection|Paginator
+    {
+        $requestFilters = $this->validateFilterableRequest->validated();
+
+        $filters = $requestFilters["filters"] ?? [];
+        $this->buildFilterableQuery($builder, $filters);
+        $this->buildSortableQuery($builder, $filters);
+
+        $filtered = $this->pagination($builder, $requestFilters);
+
+        return $filtered;
+    }
+
+    private function buildFilterableQuery(Builder $builder, array $filters): void
+    {
+        foreach ($filters as $columKeyWithFilterKey => $filterValue) {
+            // Filter keys should be in proper format. ie: __in_{column_name}.
+            // It will skip if passed filter is not in proper format/
+            preg_match("/^__[a-zA-Z]+_/", $columKeyWithFilterKey, $validKey);
+
+            if (! count($validKey)) {
+                continue;
+            }
+            $filterIdentifier = end($validKey);
+
+            // It will only filters through defined queryOperators keys.
+            if (! isset($this->queryOperators[$filterIdentifier])) {
+                continue;
+            }
+            $column = str_replace($filterIdentifier, "", $columKeyWithFilterKey);
+
+            // TODO: refactor to simpler version.
+            // Handles nested where conditions.
+            if (
+                $filterIdentifier == "__or_"
+            ) {
+                $builder->orWhere(function (Builder $query) use ($filterValue) {
+                    $this->buildFilterableQuery($query, $filterValue);
+                });
+            } elseif (
+                $filterIdentifier == "__and_"
+            ) {
+                $builder->where(function (Builder $query) use ($filterValue) {
+                    $this->buildFilterableQuery($query, $filterValue);
+                });
+            }
+
+            // It will only filter column based on defined searchable columns on model.
+            if (! in_array($column, $this->model::searchable())) {
+                continue;
+            }
+
+            $this->applyFilterQuery($filterIdentifier)
+                ->call(
+                    $this, $builder, $column, $filterValue
+                );
+        }
+    }
+
+    private function applyFilterQuery(string $operator): Closure
+    {
+        $filter = match ($operator) {
+            "__eq_", "__gt_", "__gte_",
+            "__lt_", "__lte_", "__neq_",
+            "__like_", "__nlike_", "__null_",
+            "__nnull_" => $this->defaultQueryFilter($operator),
+            "__in_" => $this->inFilter(),
+            "__nin_" => $this->notInFilter(),
+            default => $this->defaultFilter()
+        };
+
+        return $filter;
+    }
+
+    private function defaultQueryFilter(string $currentOperator): Closure
+    {
+        $queryOperator = $this->queryOperators[$currentOperator];
+        return function (Builder $builder, string $column, mixed $value) use ($queryOperator) {
+            $builder->where($column, $queryOperator, $value);
+        };
+    }
+
+    private function inFilter(): Closure
+    {
+        return function (Builder $builder, string $column, array $value) {
+            $builder->whereIn($column, $value);
+        };
+    }
+
+    private function notInFilter(): Closure
+    {
+        return function (Builder $builder, string $column, array $value) {
+            $builder->whereNotIn($column, $value);
+        };
+    }
+
+    private function defaultFilter(): Closure
+    {
+        return function (Builder $builder, string $column, mixed $value) {
+            $builder->whereLike($column, $value);
+        };
+    }
+
+    private function pagination(Builder $builder, array $requestFilters = []): Collection|Paginator
+    {
+        $perPage = (int) ($requestFilters["per_page"] ?? 25);
+        $paginate = $requestFilters["no_paginate"] ?? false;
+        $resources = !$paginate
+            ? $builder->paginate($perPage)->appends(request()->except("page"))
+            : $builder->get();
+        return $resources;
+    }
+
+    private function buildSortableQuery(Builder $builder, array $filters): void
+    {
+        // add sortable
     }
 }
