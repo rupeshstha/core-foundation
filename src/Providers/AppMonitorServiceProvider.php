d3cbdcc01c782c5c609ec6a0190962fa270bcbb8
Rupesh Shrestha <rupeshshrestha9818@gmail.com>
2024-07-02 23:07:41 +0545

    wip: binded command to service provider

--- src/Providers/AppMonitorServiceProvider.php
+++ src/Providers/AppMonitorServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CoreFoundation\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppMonitorServiceProvider extends ServiceProvider
@@ -12,5 +14,13 @@ class AppMonitorServiceProvider extends ServiceProvider
 
     public function register(): void
     {
+        // Log every single query on query.log file.
+        DB::listen(function ($query) {
+            //Write requested query on file
+            File::append(
+                path: storage_path('/logs/query.log'),
+                data: $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL
+            );
+        });
     }
 }
